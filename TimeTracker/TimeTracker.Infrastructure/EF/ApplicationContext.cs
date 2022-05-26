1359e061675fb2ecb755b5a01d10986d046d4314
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-26 12:20:46 +0300

    Added EF context

---
+++ TimeTracker/TimeTracker.Infrastructure/EF/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTracker.Core.Entities;
+
+namespace TimeTracker.Infrastructure.EF
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Record> Records { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<ActivityType> ActivityTypes { get; set; }
+    }
+}
