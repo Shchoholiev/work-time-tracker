b680aec9632be18ce87e952eb3291a98d5e28c80
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-26 11:29:55 +0300

    Added interfaces for Generic and Notes Repository.

---
+++ TimeTracker/TimeTracker.Application/IRepositories/IRecordsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+using TimeTracker.Core.Entities;
+
+namespace TimeTracker.Application.IRepositories
+{
+    public interface IRecordsRepository
+    {
+        Task AddAsync(Record entity);
+
+        Task UpdateAsync(Record entity);
+
+        Task DeleteAsync(Record entity);
+
+        Task<Record> GetOneAsync(int id);
+
+        Task<IEnumerable<Record>> GetAllAsync(Expression<Func<Record, bool>> predicate);
+
+        Task<int> GetTrackedTime(int employeeId, DateOnly date);
+
+        Task<int> GetTrackedTime(int employeeId, int weekNumber);
+    }
+}
