7df1cb4a79b7651f53498fc5dd13817ff39dd0a2
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-26 17:21:06 +0300

    Added mapper.

---
+++ TimeTracker/TimeTracker.API/Mapping/Mapper.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using TimeTracker.Application.DTO;
+using TimeTracker.Core.Entities;
+
+namespace TimeTracker.API.Mapping
+{
+    public class Mapper
+    {
+        private readonly IMapper _mapper = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<RoleDTO, Role>();
+
+            cfg.CreateMap<ActivityTypeDTO, ActivityType>();
+
+            cfg.CreateMap<RecordDTO, Record>();
+
+            cfg.CreateMap<SexDTO, Sex>();
+
+            cfg.CreateMap<EmployeeDTO, Employee>();
+
+            cfg.CreateMap<ProjectDTO, Project>();
+
+        }).CreateMapper();
+
+        public Project Map(ProjectDTO source)
+        {
+            return this._mapper.Map<Project>(source);
+        }
+
+        public Project Map(ProjectDTO source, Project destination)
+        {
+            return this._mapper.Map(source, destination);
+        }
+
+        public Employee Map(EmployeeDTO source)
+        {
+            return this._mapper.Map<Employee>(source);
+        }
+
+        public Record Map(RecordDTO source)
+        {
+            return this._mapper.Map<Record>(source);
+        }
+    }
+}
